4d6f1da75635dabeed4a345bb03cb2bb51382e0f
Huntertk <taufik.khan65@gmail.com>
2024-02-14 23:18:08 +0530

    Layout Completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/Layout"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <Layout>
+            <h1>Hello</h1>
+            </Layout>
+        } />
+
+        <Route path="/search" element={
+          <Layout>
+            <h1>Search</h1>
+            </Layout>
+        } />
+        
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
