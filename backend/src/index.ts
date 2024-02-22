210e1c0741ffb5200f731e85fbae454fff27af21
Huntertk <taufik.khan65@gmail.com>
2024-02-22 21:01:31 +0530

    Sending React file statically

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
@@ -28,12 +28,16 @@ app.use(cors({
      credentials: true
 }));
 
+app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/my-hotels", myHotelRoutes)
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+app.get("*", (req:Request, res:Response) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+})
+
 
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
