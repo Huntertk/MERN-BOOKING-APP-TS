ae615c0070e31f01996c33c477d8745e801465df
Huntertk <taufik.khan65@gmail.com>
2024-06-28 16:11:03 +0530

    Adding Deployment Scripts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/users'
 import authRoutes from './routes/auth'
+import path from 'path';
 
 //Express App Initialization
 const app = express();
@@ -22,10 +23,14 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
 }));
+app.use(express.static(path.join(__dirname, "..","..", "frontend", "dist")))
 
 //Routes
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
+app.get('*', (req:Request, res:Response) => {
+    res.status(200).sendFile(path.join(__dirname, "..", "..", "frontend", "dist", "index.html"))
+})
 
 //Server 
 app.listen(3000, () => {
