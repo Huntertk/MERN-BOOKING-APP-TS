07880a58b06d603d6175652b8c1bf961f8b1edc9
Huntertk <taufik.khan65@gmail.com>
2024-06-29 10:44:29 +0530

    cloudinary setup in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,15 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/users'
 import authRoutes from './routes/auth'
 import path from 'path';
+import {v2 as cloudinary} from 'cloudinary';
+
+
+//cloudinary
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
 
 //Express App Initialization
 const app = express();
